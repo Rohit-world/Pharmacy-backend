7b9ec9a2ab47f73ae414048ad8e9e716e2388420
xxRohitxx <rohitrs319791@gmail.com>
2023-02-12 17:36:14 +0530

    removed routes

--- models/user.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const userScema = mongoose.Schema({
-  Email: String,
-  Password: String,
-  Type:String
-});
-
-const UserModel = mongoose.model("user", userScema);
-module.exports = { UserModel };
